fc4215b1d2506535b451a0a0753a72e6a242a502
frederik von trapp <man@metbaard.nl>
2024-12-30 07:03:53 +0100

    basis werkt

--- pc-tijd.py
+++ pc-tijd.py
@@ -25,18 +25,18 @@ while True:
                 nieuwe_activiteit = oude_activiteit + time_passed
                 rows[i][1] = str(round(nieuwe_activiteit))
 
+                with open(file_obj, "w") as file:
+                    csv.writer(file).writerows(rows)
+
                 print("oude activiteit ge-update")
                 last_activity = time.time()
                 break
 
         else:
-            nieuwe_line = time.strftime("%Y_%m_%d") + " " + str(round(time_passed)) + "\n"
+            nieuwe_line = [time.strftime("%Y_%m_%d"),str(round(time_passed))]
 
-            with open("/home/saartje/exe/programmeren/python/pc_tijd_loggen/pc_tijd_logggen.txt", "a") as file:
-                file.write(nieuwe_line)
+            with open(file_obj, "a") as file:
+                csv.writer(file).writerow(nieuwe_line)
 
             print(nieuwe_line)
             last_activity = time.time()
-
-
-            github test
